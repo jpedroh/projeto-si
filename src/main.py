73e229edce9f6f19f44229e96f5170accd66dc36
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2021-06-30 21:03:39 -0300

    adicionar load data

---
+++ src/main.py
@@ -0,0 +1,23 @@
+import csv
+import sys
+
+from sklearn.model_selection import train_test_split
+
+TEST_SIZE = 0.3
+
+def main():
+    if len(sys.argv) != 2:
+        sys.exit("Usage: python main.py data")
+    
+    load_data(sys.argv[1])
+
+def load_data(filename):
+    with open(filename, encoding="utf-8") as f:
+        reader = list(csv.reader(f, delimiter="\t"))
+
+        evidence, labels = ([int(r[-1]) for r in reader], [[int(rr) for rr in r[:-1]] for r in reader])
+
+        return train_test_split(evidence, labels, test_size=TEST_SIZE, random_state=42)
+
+if __name__ == "__main__":
+    main()
